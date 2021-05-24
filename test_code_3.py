a3d99cc7c617299fe7a867444dda9143939cfe86
ashwani bandhu <ashwani@ixigo.com>
2021-05-24 18:17:07 +0530

    udated_flow

--- test_code_3.py
+++ test_code_3.py
@@ -1,23 +1,23 @@
 from prefect import task, Flow
 from prefect.storage import GitHub
 
+
 @task
 def get_data():
     return [1, 2, 3, 4, 5]
 
+
 @task
 def print_data(data):
     print(data)
+
 
 with Flow("example") as flow:
     data = get_data()
     print_data(data)
 
 flow.storage = GitHub(
-    repo="Learning-ML",                            # name of repo
-    path="/test_code_3.py",
+    repo="ashwani021994/Learning-ML",  # name of repo
+    path="/test_code_3.py"
 
 )
-
-
-
